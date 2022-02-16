783655d76fc3b73ef5559adaf5ad0f088c7731fc
Your NameMichelle <watts.e.michelle@gmail.com>
2022-02-16 20:14:14 +1100

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  // publicPath: process.env.NODE_ENV === "production" ? "/portfolio-vue/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-vue/" : "/",
   devServer: {
     host: "localhost",
     port: 8080,
