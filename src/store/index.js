67facd2ffcf34c2d8c14c01c34f698bfb381286a
Your NameMichelle <watts.e.michelle@gmail.com>
2022-02-18 16:45:03 +1100

    updated data to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
   state: {
-    message: "",
+    loading: false,
+    waveArray: [],
     count: 0,
-    shopNumber: 2,
     todos: [
       { text: "Learn JavaScript" },
       { text: "Learn Vue" },
       { text: "Build something awesome" },
     ],
-
     categories: [
       "sustainability",
       "nature",
@@ -27,6 +27,10 @@ export default new Vuex.Store({
     colors: ["red", "blue", "green", "black", "yellow", "purple", "white"],
     objects: ["domain", "star", "home", "car"],
     sizes: ["10", "25", "50", "75", "100"],
+    posts: [],
+    result: "",
+    message: "",
+    apiKey: "bcfe6b08dcmsh6ed06ffa591d054p14d4b0jsn565c23c38d4f",
   },
   getters: {
     doubleCount(state) {
@@ -35,16 +39,73 @@ export default new Vuex.Store({
     shop(state) {
       return "The shop number is " + state.shopNumber;
     },
+    loading(state) {
+      return state.loading;
+    },
+    result(state) {
+      return state.result;
+    },
   },
   mutations: {
     increment(state) {
       state.count++;
     },
+    updateMessage(state, message) {
+      state.message = message;
+    },
     decrement(state) {
       state.count--;
     },
+    reverse(state) {
+      state.message = state.message.split("").reverse().join("");
+    },
+    dash(state) {
+      state.message = state.message.split("").join("-");
+    },
+    vowels(state) {
+      let reg = /[aeiou]/gi;
+      let split = state.message.split("");
+      let vowel = split.filter((item) => {
+        return !item.match(reg);
+      });
+      state.message = vowel.join("");
+    },
+    wave(state) {
+      for (let i = 0; i < state.message.length; i++) {
+        let letter = state.message[i];
+        if (letter === " ") {
+          continue;
+        } else {
+          state.waveArray.push(
+            state.message.slice(0, i) +
+              letter.toUpperCase() +
+              state.message.slice(i + 1)
+          );
+        }
+      }
+      return state.waveArray;
+    },
+    clear(state) {
+      state.message = "";
+      state.waveArray = [];
+    },
+
     timesTwo(state) {
       state.count = state.count * 2;
+    },
+    vowel(state) {
+      let reg = /[aeiou]/gi;
+      let split = state.message.split("");
+      let vowel = split.filter((item) => {
+        return !item.match(reg);
+      });
+      state.message = vowel.join("");
+    },
+    SET_RESULT(state, result) {
+      state.result = result;
+    },
+    loading(state, newLoading) {
+      state.loading = newLoading;
     },
   },
   actions: {
@@ -53,6 +114,24 @@ export default new Vuex.Store({
     },
     timesTwo({ commit }) {
       commit("timesTwo");
+    },
+    getJoke({ commit }) {
+      commit("loading", true);
+
+      axios
+        .get("https://funny-joke-dataset.p.rapidapi.com/users", {
+          headers: {
+            "x-rapidapi-host": "funny-joke-dataset.p.rapidapi.com",
+            "x-rapidapi-key":
+              "bcfe6b08dcmsh6ed06ffa591d054p14d4b0jsn565c23c38d4f",
+          },
+        })
+        .then((response) => {
+          let find = response.data.users[Math.floor(Math.random() * 1000)].body;
+          console.log(find);
+          commit("loading", false);
+          commit("SET_RESULT", find);
+        });
     },
   },
   modules: {},
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -2,72 +2,46 @@
   <v-container my-10>
     <v-row class="mx-5" align="center" justify="center">
       <v-col lg="8" md="8" sm="12" xs="12">
-        <h3 class="mb-5 title" v-if="responseAvailable == true">
+        <div>
           {{ result }}
-        </h3>
+        </div>
         <v-btn
-          color="primary"
+          class="mt-5 subtitle-1 primary"
           :loading="loading"
-          class="mb-5 subtitle-1"
-          id="get-joke"
-          @click="fetchAPIData"
-          >Get Joke
-        </v-btn>
-        <!-- 
-        <v-card outlined>
-          <v-list-item three-line>
-            <v-list-item-content>
-              <v-list-item-title class="text-h5 mb-1">
-                Joke of the day
-              </v-list-item-title>
-              <v-list-item-subtitle v-if="responseAvailable == false"
-                >A duck walks into a bar...</v-list-item-subtitle
-              >
-              <v-list-item-subtitle v-if="responseAvailable == true">{{
-                result
-              }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+          @click="getJoke"
+          >Get Joke 2.0</v-btn
+        >
 
-          <v-card-actions>
-            <v-btn
-              color="primary"
-              :loading="loading"
-              id="get-joke"
-              @click="fetchAPIData"
-              >Get Joke
-            </v-btn>
-          </v-card-actions>
-        </v-card> -->
-        <v-divider></v-divider>
-        <h1 class="mt-10">Word Play</h1>
+        <div class="mt-10 display-1">Word Play</div>
         <v-text-field v-model="message" label="type anything" type="text">
         </v-text-field>
         <h2 class="mb-5">{{ message }}</h2>
         <h2 class="mb-5" v-if="waveArray.length > 0">{{ waveArray }}</h2>
-        <v-btn class="mr-5" @click="wave">Make a word wave</v-btn>
-        <v-btn class="mr-5" @click="reverseMessage">Reverse String </v-btn>
-        <v-btn class="mr-5" @click="dashMessage">Dash String </v-btn>
-        <v-btn class="mr-5" @click="zeroVowels">No vowels </v-btn>
+        <v-btn class="mb-5" @click="makeWave">Make a word wave</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn class="mb-5" @click="reverseMessage">Reverse String </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn class="mb-5" @click="dashMessage">Dash String </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn class="mb-5" @click="zeroVowels">No vowels </v-btn>
+        <v-spacer></v-spacer>
         <v-btn @click="clearMessage">Clear</v-btn>
 
         <Icon class="mt-10" :color="color" :object="object" :size="size" />
-        <h3 class="mt-10">Change the type and colour of the icon</h3>
+        <div class="mt-10 display-1">
+          Change the type and colour of the icon
+        </div>
         <v-select v-model="object" :items="objects" label="Objects"></v-select>
         <v-select v-model="color" :items="colors" label="Colors"></v-select>
 
         <v-select v-model="size" :items="sizes" label="Sizes"></v-select>
 
-        <h1 class="mt-10">Numbers</h1>
+        <div class="display-1 mt-10">Numbers</div>
         <h1>{{ count }}</h1>
-        <!-- <h1>{{ doubleCount }}</h1> -->
 
         <v-btn class="mr-5" @click="increment"> + </v-btn>
         <v-btn class="mr-5" @click="decrement"> - </v-btn>
         <v-btn @click="timesTwo"> x2 </v-btn>
-
-        <!-- <h1>{{ number }}</h1>
-        <v-btn @click="totalMarks"> x2 </v-btn> -->
       </v-col>
     </v-row>
   </v-container>
@@ -82,19 +56,9 @@ export default {
   name: "Test",
   data() {
     return {
-      button_text: "Submit",
-      loaderActive: false,
-      loading: false,
       color: "",
-      array: ["item1", "item2", "item3"],
-      message: "",
-      number: 2,
       size: "50",
       object: "domain",
-      waveArray: [],
-      result: "",
-      responseAvailable: false,
-      apiKey: "bcfe6b08dcmsh6ed06ffa591d054p14d4b0jsn565c23c38d4f",
     };
   },
   computed: {
@@ -107,49 +71,20 @@ export default {
       "colors",
       "objects",
       "sizes",
+      "result",
+      "waveArray",
     ]),
-    ...mapGetters(["doubleCount", "shop"]),
+    ...mapGetters(["doubleCount", "shop", "loading", "result"]),
+    message: {
+      get() {
+        return this.$store.state.message;
+      },
+      set(value) {
+        this.$store.commit("updateMessage", value);
+      },
+    },
   },
   methods: {
-    showLoader() {
-      this.loaderActive = true;
-    },
-    hideLoader() {
-      this.loaderActive = false;
-    },
-    fetchAPIData() {
-      this.responseAvailable = false;
-      // this.button_text = `Loading`;
-      // this.showLoader();
-      this.loading = true;
-      fetch("https://funny-joke-dataset.p.rapidapi.com/users", {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "funny-joke-dataset.p.rapidapi.com",
-          "x-rapidapi-key": this.apiKey,
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            alert(
-              "Server returned " + response.status + " : " + response.statusText
-            );
-          }
-        })
-        .then((response) => {
-          console.log(response.users[Math.floor(Math.random() * 1000)].body);
-          this.result = response.users[Math.floor(Math.random() * 1000)].body;
-          this.responseAvailable = true;
-          // this.button_text = "Submit";
-          // this.hideLoader();
-          this.loading = false;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
     increment() {
       this.$store.dispatch("increment");
     },
@@ -159,41 +94,23 @@ export default {
     timesTwo() {
       this.$store.commit("timesTwo");
     },
-    reverseMessage: function () {
-      this.message = this.message.split("").reverse().join("");
+    reverseMessage() {
+      this.$store.commit("reverse");
     },
-    dashMessage: function () {
-      this.message = this.message.split("").join("-");
+    dashMessage() {
+      this.$store.commit("dash");
     },
-    clearMessage: function () {
-      this.message = "";
-      this.waveArray = [];
+    clearMessage() {
+      this.$store.commit("clear");
     },
-    totalMarks: function () {
-      this.number = this.number * 2;
+    getJoke() {
+      this.$store.dispatch("getJoke");
     },
-    zeroVowels: function () {
-      let reg = /[aeiou]/gi;
-      let split = this.message.split("");
-      let vowel = split.filter((item) => {
-        return !item.match(reg);
-      });
-      this.message = vowel.join("");
+    zeroVowels() {
+      this.$store.commit("vowels");
     },
-    wave: function () {
-      for (let i = 0; i < this.message.length; i++) {
-        let letter = this.message[i];
-        if (letter === " ") {
-          continue;
-        } else {
-          this.waveArray.push(
-            this.message.slice(0, i) +
-              letter.toUpperCase() +
-              this.message.slice(i + 1)
-          );
-        }
-      }
-      return this.waveArray;
+    makeWave() {
+      this.$store.commit("wave");
     },
   },
 
@@ -202,14 +119,13 @@ export default {
       return value.toFixed(2);
     },
   },
-  mounted() {},
 };
 </script>
 
 <style scoped lang="scss">
-.v-btn:not(.v-btn--round).v-size--default {
-  color: $white;
-  background-color: $ads-navy;
-  font-weight: 900;
-}
+// .v-btn:not(.v-btn--round).v-size--default {
+//   color: $white;
+//   background-color: $ads-navy;
+//   font-weight: 900;
+// }
 </style>
