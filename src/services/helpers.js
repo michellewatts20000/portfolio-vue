04391f9d80727d69165a01ed07d772873fc347ba
Michelle.Watts22 <michelle.watts22@det.nsw.edu.au>
2023-06-02 13:10:46 +1000

    dist folder

--- src/services/helpers.js
+++
@@ -1,7 +0,0 @@
-export default {
-  formatDate(value) {
-    if (value) {
-      return moment(String(value)).format("MMMM Do YYYY");
-    }
-  },
-};
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-vue/" : "/",
-  devServer: {
-    host: "localhost",
-    port: 8080,
-  },
-};
