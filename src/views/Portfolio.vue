3a0ba6572fd9f0d8e83e8c263dc660386492f9e8
Michelle.Watts22 <michelle.watts22@det.nsw.edu.au>
2023-06-06 10:34:37 +1000

    updates

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -10,7 +10,8 @@
       >
         <v-img height="200" :src="require(`@/assets/${card.url}`)"></v-img>
         <v-card-title class="title">{{ card.name }}</v-card-title>
-        <v-card-text class="body-2">
+        <v-card-subtitle >{{ card.tech }}</v-card-subtitle>
+        <v-card-text class="body-1">
           <div>
             {{ card.description }}
           </div>
@@ -35,6 +36,7 @@ export default {
       cards: [
       {
           name: 'Promptopia',
+          tech: 'Next.js, MongoDB, Tailwind CSS',
           url: 'note.jpg',
           description:
             'Create, edit and delete prompts for AI. Built with Next.js, MongoDB and Tailwind CSS.',
@@ -44,6 +46,7 @@ export default {
         
         {
           name: "Hearo",
+          tech: 'React, MongoDB, Express, Node.js',
           url: 'hearo.png',
           description:
             "Find quiet places to socialise in Sydney. An app that lets you search for quiet places and review places for their loudness.",
@@ -52,14 +55,16 @@ export default {
         },
         {
           name: "What's in my Pantry?",
+          tech: 'Typescript, React, Material UI, REST API',
           url: 'pantry.jpg',
           description:
             "What's in my pantry is an easy to use, all-in-one recipe search app that will display recipes based on a list of food items that are currently in your pantry.",
-          deployed: 'https://michellewatts20000.github.io/whats-in-my-pantry/',
-          github: 'https://github.com/michellewatts20000/whats-in-my-pantry',
+          deployed: 'https://voluble-blini-cd8074.netlify.app/',
+          github: 'https://github.com/michellewatts20000/react-pantry',
         },
         {
           name: 'Overtime Calculator',
+          tech: 'HTML, CSS, JavaScript',
           url: 'unpaid-ot.jpg',
           description:
             "A full-stack app that calculates the amount of unpaid hours and salary you have worked over one year. It's more then you think.",
@@ -68,6 +73,7 @@ export default {
         },
         {
           name: 'Tech Blog',
+          tech: 'Handlebars, MySQL, Express.js, Node.js',
           url: 'blogger.jpg',
           description:
             'A full-stack app that lets you write and comment about tech related issues. Create an account, check out other peoples blogs.',
@@ -76,6 +82,7 @@ export default {
         },
         {
           name: 'Weather Dashboard',
+          tech: 'HTML, CSS, JavaScript, REST API',
           url: 'weather.jpg',
           description:
             'The weather dashboard is an app that gives you the current weather and UV index for a place you search for and a 5 day forecast.',
